3e9f5bbd838cdeff57097a4573a3e86f0de9d742
coskun cakir <csknckr@gmail.com>
2021-02-15 17:05:47 +0000

    loading state added

--- src/app/characters/characters.component.ts
+++ src/app/characters/characters.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SwapiService } from 'src/app/service/swapi/swapi.service';
 import { ICharacter } from '../service/swapi/swapi.interface';
-
+import { take } from 'rxjs/operators';
 @Component({
   selector: 'app-characters',
   templateUrl: './characters.component.html',
@@ -11,11 +11,16 @@ export class CharactersComponent implements OnInit {
   constructor(private swapiService: SwapiService) {}
 
   characters: ICharacter[] = [];
+  loading = true;
 
   ngOnInit(): void {
-    this.swapiService.characters().subscribe((response) => {
-      this.characters = response.results;
-    });
+    this.swapiService
+      .characters()
+      .pipe(take(1))
+      .subscribe((response) => {
+        this.characters = response.results;
+        this.loading = false;
+      });
   }
 
   goToCharacterDetail(character: ICharacter): void {
