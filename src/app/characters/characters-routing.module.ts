ca0e489bd86dae71c3bcfcd9ec345b86e634196b
coskun cakir <csknckr@gmail.com>
2021-02-15 14:01:01 +0000

    tailwind css added

---
+++ src/app/characters/characters-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CharacterComponent } from './character/character.component';
+import { CharactersComponent } from './characters.component';
+
+const routes: Routes = [
+  { path: '', component: CharactersComponent },
+  { path: ':id', component: CharacterComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class CharactersRoutingModule {}
